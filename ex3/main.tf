2328281980827e476adbf14169481e8841661e60
Tomasz <tomasz.exchange.place@gmail.com>
2023-10-16 16:33:08 +0200

    ISSUE-8: Compute + Network ready

--- ex3/main.tf
+++ ex3/main.tf
@@ -9,18 +9,46 @@ resource "openstack_compute_instance_v2" "nginxnode" {
   key_pair    = openstack_compute_keypair_v2.this_provided.name
   #user_data   = file("scripts/first-boot.sh")
   region = var.region_name
+
   network {
+    uuid = openstack_networking_network_v2.nginxnode.id
     port = openstack_networking_port_v2.nginxnode.id
   }
+
 }
 
 #------------------------------------------------------------------------------
 # networking
 #------------------------------------------------------------------------------
 
+data "openstack_networking_network_v2" "generic" {
+  name = var.external_network
+}
+
+resource "openstack_networking_router_v2" "nginxnode" {
+  name                = "${var.name}_router"
+  external_network_id = data.openstack_networking_network_v2.generic.id
+}
+
+resource "openstack_networking_network_v2" "nginxnode" {
+  name = "${var.name}_network"
+}
+
+resource "openstack_networking_subnet_v2" "nginxnode" {
+  name            = var.network_nginxnode["subnet_name"]
+  network_id      = openstack_networking_network_v2.nginxnode.id
+  cidr            = var.network_nginxnode["cidr"]
+  dns_nameservers = var.dns_ip
+}
+
+resource "openstack_networking_router_interface_v2" "nginxnode" {
+  router_id = openstack_networking_router_v2.nginxnode.id
+  subnet_id = openstack_networking_subnet_v2.nginxnode.id
+}
+
 resource "openstack_networking_port_v2" "nginxnode" {
   name           = "port-instance-nginxnode"
-  network_id     = openstack_networking_network_v2.generic.id
+  network_id     = openstack_networking_network_v2.nginxnode.id
   admin_state_up = true
   security_group_ids = [
     openstack_compute_secgroup_v2.ssh.id,
@@ -32,12 +60,13 @@ resource "openstack_networking_port_v2" "nginxnode" {
 }
 
 resource "openstack_networking_floatingip_v2" "nginxnode" {
-  pool = var.external_network
+  pool = data.openstack_networking_network_v2.generic.name
 }
 
 resource "openstack_compute_floatingip_associate_v2" "nginxnode" {
   floating_ip = openstack_networking_floatingip_v2.nginxnode.address
   instance_id = openstack_compute_instance_v2.nginxnode.id
+  fixed_ip    = openstack_compute_instance_v2.nginxnode.network[0].fixed_ip_v4
 }
 
 #------------------------------------------------------------------------------
--- ex3/variables.tf
+++ ex3/variables.tf
@@ -6,11 +6,6 @@ variable "external_network" {
   default = "Ext-Net"
 }
 
-variable "external_gateway" {
-  type    = string
-  default = "f67f0d72-0ddf-11e4-9d95-e1f29f417e2f"
-}
-
 variable "dns_ip" {
   type    = list(string)
   default = ["8.8.8.8", "8.8.8.4"]
@@ -22,7 +17,7 @@ variable "dns_ip" {
 
 variable "image" {
   type    = string
-  default = "Centos 8"
+  default = "Centos 7"
 }
 
 variable "region_name" {
@@ -48,7 +43,7 @@ variable "network_nginxnode" {
 variable "name" {
   default     = "nginxnode"
   type        = string
-  description = "KeyPair name to create"
+  description = "Specif prefix for resources (e.g. key, router)"
 }
 
 variable "generate_ssh_key" {
